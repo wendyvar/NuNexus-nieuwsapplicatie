95236e285f5d065f52e681a2a12d423f98349d02
wendyvar <101524469+wendyvar@users.noreply.github.com>
2024-03-19 17:19:42 +0100

    opzet

---
+++ src/components/NewsBoard.jsx
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import NewsItem from "./NewsItem";
+const NewsBoard = () => {
+
+    const [articles,setArticles] = useState([]);
+
+    useEffect(()=>{
+        let url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=${import.meta.env.VITE_API_KEY}';
+        fetch(url).then(response=> response.json()).then(data=> setArticles(data.articles));
+
+
+    },[])
+
+  return (
+    <div>
+      <h2 className="text-center"> Latest <span className=""> News</span></h2>
+    {articles.map((news,index)=>{
+        return <NewsItem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url}/>
+    })}
+    
+    
+    </div>
+  )
+}
+
+export default NewsBoard
---
+++ src/components/NewsItem.jsx
@@ -0,0 +1,18 @@
+
+
+
+
+const NewsItem = ({title, description, src, url }) => {
+  return (
+    <div className="card" style={{maxWidth:"345px"}}>
+  <img src={src} className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">{title}</h5>
+    <p className="card-text">{description}</p>
+    <a href={url} className="btn btn-primary">Lezen</a>
+  </div>
+</div>
+  )
+}
+
+export default NewsItem;
